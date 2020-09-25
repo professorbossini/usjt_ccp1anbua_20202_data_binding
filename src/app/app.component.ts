de95dc55bb885b7ca5ed512bb75374e702f0d4a0
Rodrigo Bossini <professorbossini@gmail.com>
2020-09-24 21:41:38 -0300

    Terminamos o pdf 05-02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'usjt-ccp1bua-data-binding';
+
+  nome: string;
+  exibeCaixa: boolean = false;
+
+  alterarNome (nome): void{
+    console.log(nome.target.value);
+    this.nome = nome.target.value;
+  }
+
+  adicionar(nomeInput): void {
+    console.log (nomeInput.value);
+    this.nome = nomeInput.value;
+    this.exibeCaixa = true;
+    //console.log('Adicionando...');
+  }
+
+  /*nome: string = "José"
+  idade: number = 20
+
+  lancarDado (): number{
+    return Math.floor(Math.random() * 6) + 1;
+  }*/
 }
